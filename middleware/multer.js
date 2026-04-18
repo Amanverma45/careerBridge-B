a5da61e0ea253f5951556703e645d9fbc244d5cd
Aman Verma <av478136@gmail.com>
2026-04-19 00:16:44 +0530

    change req,file.path

--- middleware/multer.js
+++ middleware/multer.js
@@ -6,7 +6,7 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: "resumes",
-    resource_type: "auto", 
+    resource_type: "raw", 
     
     allowed_formats: ['pdf', 'doc', 'docx'], 
   }
