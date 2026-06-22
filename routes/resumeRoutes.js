15f3f05ddd85ba413364efc08c929d0fbe790c40
Aman Verma <av478136@gmail.com>
2026-06-22 23:52:42 +0530

    make view resume api

--- routes/resumeRoutes.js
+++ routes/resumeRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express")
+const resumeController = require('../controller/resumeController.js')
 const router = express.Router()
 
 const upload = require("../middleware/multer")
 
 const { uploadResume, deleteResume } = require("../controller/resumeController")
 
-router.post("/uploadResume", upload.single("resume"), uploadResume)
+router.post("/uploadResume", upload.single("resume"), resumeController.uploadResume)
 router.get("/viewResume/:id", resumeController.viewResume);
-router.delete("/deleteResume/:id", deleteResume)
+router.delete("/deleteResume/:id", resumeController.deleteResume)
 
 module.exports = router
