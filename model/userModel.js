9f2299cdbb001420c5bb5db51653355deaba3db1
Aman Verma <av478136@gmail.com>
2026-08-01 15:32:15 +0530

    clean userModel schema defaults to prevent automatic insertion of resume and company fields across roles

--- model/userModel.js
+++ model/userModel.js
@@ -7,26 +7,11 @@ const userSchema = new mongoose.Schema({
     skills: String,
     experience: String,
     bio: String, 
-    companyName: {
-        type: String,
-        default: ""
-    },
-    companyWebsite: {
-        type: String,
-        default: ""
-    },
-    companyDescription: {
-        type: String,
-        default: ""
-    },
-    resume: {
-        type: String,
-        default: ""
-    },
-    resumePublicId: {
-    type: String,
-    default: ""
-}
+    companyName: String,
+    companyWebsite: String,
+    companyDescription: String,
+    resume: String,
+    resumePublicId: String
 })
 const user = new mongoose.model('user', userSchema)
 module.exports = user
